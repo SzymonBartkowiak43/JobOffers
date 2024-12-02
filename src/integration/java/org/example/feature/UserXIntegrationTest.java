787f6d44e53b028967eaa7481c86cdc4e1ed02b4
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-12-02 18:17:22 +0100

    Add spring boot, prepare class for integration test, and create docker compose

---
+++ src/integration/java/org/example/feature/UserXIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.example.feature;
+
+import org.example.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+public class UserXIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void f() {
+
+    }
+}
